8902d7b142a2f0425a0ad4097e44780da4db3f5a
pingflood <pingflood@gmail.com>
2018-06-14 21:23:24 -0300

    Improve menu combo button logic

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -1328,14 +1328,10 @@ bool GMenu2X::inputCommonActions(bool &inputAction) {
 
 	bool wasActive = false;
 	while (input[POWER]) {
-		WARNING("POWER HOLD");
-
 		wasActive = true;
-
 		input.update();
-
-		// HOLD POWER BUTTON
 		if (input[POWER]) {
+			// HOLD POWER BUTTON
 			powerManager->doPowerOff(0, NULL);
 			return true;
 		}
@@ -1346,24 +1342,23 @@ bool GMenu2X::inputCommonActions(bool &inputAction) {
 		return true;
 	}
 
-	wasActive = false;
 	while (input[MENU]) {
-		input.update();
 		wasActive = true;
+		input.update();
 		if (input[SECTION_NEXT]) {
 			// SCREENSHOT
 			if (!saveScreenshot()) { ERROR("Can't save screenshot"); return true; }
 			MessageBox mb(this, tr["Screenshot saved"]);
-			mb.setAutoHide(500);
+			mb.setAutoHide(1000);
 			mb.exec();
-			wasActive = false;
+			return true;
 		} else if (input[SECTION_PREV]) {
 			// VOLUME / MUTE
 			setVolume(confInt["globalVolume"], true);
-			wasActive = false;
+			return true;
 		}
 	}
-
+	
 	input[MENU] = wasActive; // Key was active but no combo was pressed
 
 	if ( input[BACKLIGHT] ) {
