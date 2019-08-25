09e53774d811f546ccc15f3b5e3ec9e54d051dd2
pingflood <pingflood@gmail.com>
2019-08-25 11:36:38 -0300

    Accept params in alias file

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -35,7 +35,7 @@
 
 // for browsing the filesystem
 #include <sys/stat.h>
-#include <sys/types.h>
+// #include <sys/types.h>
 #include <dirent.h>
 
 // for soundcard
--- src/linkapp.cpp
+++ src/linkapp.cpp
@@ -17,11 +17,11 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-#include <sys/types.h>
+// #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fstream>
-#include <sstream>
+// #include <sstream>
 
 #include "linkapp.h"
 #include "menu.h"
@@ -320,6 +320,8 @@ void LinkApp::selector(int startSelection, const string &selectorDir) {
 			save();
 		}
 
+		params = trim(params + " " + bd.getParams(bd.selected));
+
 		launch(bd.getFile(bd.selected), bd.getPath());
 	}
 }
@@ -330,8 +332,6 @@ void LinkApp::launch(const string &selectedFile, string dir) {
 	mb.exec();
 
 	gmenu2x->setVolume(gmenu2x->confInt["globalVolume"]);
-
-	save();
 
 	// Set correct working directory
 	string wd = getRealWorkdir();
